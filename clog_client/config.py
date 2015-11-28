d3ca300b86bbd22e914434408f846cdf4b170129
Ivan Mirić <imiric@gmail.com>
2015-11-28 19:55:13 +0100

    Add rudimentary error handling

--- clog_client/config.py
+++ clog_client/config.py
@@ -6,4 +6,5 @@ class Config:
     host = os.getenv('CLOG_HOST', 'localhost')
     port = os.getenv('CLOG_PORT', '5000')
     server_url = 'http://{}:{}'.format(host, port)
+    request_timeout = os.getenv('CLOG_TIMEOUT', 1)
     source = os.getenv('CLOG_SOURCE', socket.gethostname())
--- clog_client/core.py
+++ clog_client/core.py
@@ -1,10 +1,22 @@
+import warnings
+
 import requests
 
 from .config import Config
 
 
+class ClogRequestWarning(Warning):
+    pass
+
+
 def log(data, metadata={}):
     payload = {'log': {'data': data, 'metadata': metadata},
                'source': Config.source}
-    return requests.post('{}/api/v1/logs/'.format(Config.server_url),
-                         data=payload)
+    try:
+        res = requests.post('{}/api/v1/logs/'.format(Config.server_url),
+                            data=payload, timeout=Config.request_timeout)
+    except requests.exceptions.RequestException as e:
+        # TODO: Add retry and/or failover/queue mechanism.
+        warnings.warn(str(e), ClogRequestWarning)
+
+    return res
